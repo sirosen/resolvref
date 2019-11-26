bf1568f45e7e3242ff226db6fa8877398fad412e
Stephen Rosen <sirosen@globus.org>
2019-11-26 17:50:23 +0000

    Use yaml.safe_load in tests
    Suppresses warnings, does no harm.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,7 +9,7 @@ def test_simple_file_expansion_yaml(simple_yaml_filename, tmpdir):
     outputfile = str(tmpdir.join("dest.yaml"))
     main([simple_yaml_filename, "-o", outputfile])
     with open(outputfile, "r") as fp:
-        result = yaml.load(fp)
+        result = yaml.safe_load(fp)
     assert result == {
         "part1": {
             "k1": "a string",
@@ -55,7 +55,7 @@ def test_simple_file_expansion_json_loaded_as_yaml(simple_json_filename, tmpdir)
     outputfile = str(tmpdir.join("dest.yaml"))
     main([simple_json_filename, "-o", outputfile, "-f", "yaml"])
     with open(outputfile, "r") as fp:
-        result = yaml.load(fp)
+        result = yaml.safe_load(fp)
     assert result == {
         "part1": {
             "k1": "a string",
